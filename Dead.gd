dbec3dd07cda13e8f4821ef2898802092b277269
yassin <ymennana@elpuig.xeill.net>
2024-03-24 13:53:26 +0100

    He cambiado un poco el mapa de street, tambien he encontrado un bug y es que cuando te matan respawneas en BedRoom pero fuera, ya esta solucionado

--- Dead.gd
+++ Dead.gd
@@ -1,7 +1,13 @@
 extends Control
 
+@export var target_position: Vector2
+@export var target_scene: String
+@export var target_zoom: Vector2
 
 
 
 func _on_respawn_pressed():
-	get_tree().change_scene_to_file("res://Scenes/menu.tscn")
+	global.posicion = target_position
+	global.zoom = target_zoom
+	get_tree().change_scene_to_file("res://Scenes/" + target_scene + ".tscn")
+	return
---
+++ Scenes/SpawnTrasMuerte.gd
@@ -0,0 +1,14 @@
+extends Button
+
+@export var target_position: Vector2
+@export var target_scene: String
+@export var target_zoom: Vector2
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
