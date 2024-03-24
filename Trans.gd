4e5f88ce089a7cbd5166ee72d0f87240725c96ce
yassin <ymennana@elpuig.xeill.net>
2024-03-25 00:00:25 +0100

    Demo del juego clean

--- Trans.gd
+++ Trans.gd
@@ -2,12 +2,14 @@ extends CanvasLayer
 
 
 func _ready():
-	layer = -1
-	$AnimationPlayer.play("trans")
+	#layer = -1
+	#$AnimationPlayer.play("trans")
+	pass
 func trans_scene():
-	layer = 1
-	$AnimationPlayer.play("trans")
-	await ($AnimationPlayer. animation_finished)
-	$AnimationPlayer.play_backwards("trans")
-	await ($AnimationPlayer. animation_finished)
-	layer = -1
+	#layer = 1
+	#$AnimationPlayer.play("trans")
+	#await ($AnimationPlayer. animation_finished)
+	#$AnimationPlayer.play_backwards("trans")
+	#await ($AnimationPlayer. animation_finished)
+	#layer = -1
+	pass
