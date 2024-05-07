b82a449f4e60a004427e8dc257c3cf8648d5af6d
Frnkp21 <perezcarvajalfrank@elpuig.xeill.net>
2024-05-07 20:51:18 +0200

    dialogo a la mitad

---
+++ Scripts/Dialogo.gd
@@ -0,0 +1,43 @@
+extends Control
+
+
+signal dialogo_finish
+
+@export_file("*.json") var d_file
+
+var dialogo = []
+var current_dialogo_id = 0
+var d_active = false 
+
+func _ready():
+	$NinePatchRect.visible = false
+
+func start():
+	if d_active:
+		return
+	d_active = true
+	dialogo = load_dialogo()
+	current_dialogo_id = -1
+	next_script()
+
+func load_dialogo():
+	var file = FileAccess.open("res://Dialog/portal_dialogo1.json", FileAccess.READ)
+	var content = JSON.parse_string(file.get_as_text())
+	return content
+
+func _input(event):
+	if !d_active:
+		return
+	if event.is_action_pressed("ui_accept"):
+		next_script()
+
+func next_script():
+	current_dialogo_id += 1
+	if current_dialogo_id >= len(dialogo):
+		d_active = false
+		$NinePatchRect.visible = false
+		emit_signal("dialogo_finish")
+		return
+		
+	$NinePatchRect/name.text = dialogo[current_dialogo_id]['name']
+	$NinePatchRect/text.text = dialogo[current_dialogo_id]['text']
--- Scripts/Portal.gd
+++ Scripts/Portal.gd
@@ -1,5 +1,12 @@
 extends CharacterBody2D
 
+
+var is_roaming = true
+var is_chatting = false
+
+var player
+var player_in_chat_zone = false
+
 func _ready():
 	$AnimatedSprite2D.play("Idle")
 
@@ -8,3 +15,28 @@ func portalInicio():
 
 func interactuable():
 	pass
+func choose(array):
+	array.shuffle()
+	return array.front()
+	
+
+
+func _on_chatdetection_area_body_entered(body):
+	if body.has_method("player"):
+		player = body
+		player_in_chat_zone = true
+
+
+func _on_chatdetection_area_body_exited(body):
+	if body.has_method("player"):
+		player_in_chat_zone = false
+
+func _process(delta):
+	if Input.is_action_just_pressed("chat"):
+		print("chatting")
+		is_chatting = true
+
+func _on_control_dialogo_finish():
+	is_chatting = false
+	is_roaming = true
+ 
